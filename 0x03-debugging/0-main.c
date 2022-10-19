7fcf1a9df2c1409e70a7d6f97cb29b56e2137331
Folajomi1 <adesolaadeshina112@outlook.com>
2022-10-19 14:35:22 -0700

    my second commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int i;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-i = 0;
-positive_or_negative(i);
-
-return (0);
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
