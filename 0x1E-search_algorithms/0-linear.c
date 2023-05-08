c7cddc4180f3e5795750e537724d0c5e85625f22
Folajomi1 <adesolaadeshina112@outlook.com>
2023-05-08 07:01:36 -0700

    my first commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+    if (array == NULL)
+        return -1;
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+        if (array[i] == value)
+            return i;
+    }
+
+    return -1;
+}
+
+int main(void)
+{
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+
+    return 0;
+}
+
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,3 @@
+C - Search Algorithms
+
+This project focuses on learning and implementing various search algorithms in the C programming language. The goal is to understand the concepts of search algorithms, including linear search and binary search, and determine the most suitable algorithm based on specific needs.
